97e5f6cc81df5186456e1d068221015d8186245b
arfrhmnshrtn <iw8628940@gmail.com>
2023-08-15 01:14:16 +0700

    memperbaiki fitur dark mode

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -9,6 +9,10 @@ import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import DataObjectIcon from '@mui/icons-material/DataObject';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import NightlightOutlinedIcon from '@mui/icons-material/NightlightOutlined';
+import Brightness5OutlinedIcon from '@mui/icons-material/Brightness5Outlined';
 
 
 import React, { useState } from 'react';
@@ -41,10 +45,10 @@ export default function Navbar() {
 
     return (
         <>
-            <div className="flex">
+            <div className={`flex h-screen ${darkMode ? 'dark' : ''}`}>
 
 
-                <div className={`h-screen ${darkMode ? 'bg-slate-700 text-white' : ''}`}>
+                <div className={`bg-white dark:bg-slate-700 dark:text-white`}>
                     <h1 className="text-xl font-bold text-center py-5 ">
                         <span className="">{showText ? (
                             <span className="ml-2 "><a href=''><DataObjectIcon />RifDev</a></span>
@@ -52,8 +56,8 @@ export default function Navbar() {
                             <span className="ml-2"><a href=''><DataObjectIcon /></a></span>
                         )}</span>
                     </h1>
-                    <div className='dark:bg-black'>
-                        <ul className=" py-5 divide-y divide-slate-200 ">
+                    <div className='dark:bg-slate-700'>
+                        <ul className=" py-5 divide-y divide-slate-200 dark:divide-slate-800">
                             <li
                                 className={`py-2 px-5 w-max cursor-pointer ${isi === 'Dashboard' ? 'text-red-600' : 'hover:text-red-600'
                                     }`}
@@ -114,12 +118,12 @@ export default function Navbar() {
                         </button>
                     </div>
                 </div>
-                <div className={` h-max w-full py-4 px-2 ${darkMode ? 'bg-slate-700 text-white' : ''} `}>
+                <div className={` h-max w-full py-4 px-2 dark:bg-slate-700 dark:text-white `}>
                     <div className='flex justify-between pb-3'>
                         <div className='flex '>
                             <button onClick={buttonHandler}><MenuOutlinedIcon sx={{ fontSize: 30 }} /></button>
                             <div className='flex items-center'>
-                                <input type="text" name="cari" id="" className='ms-2 outline-none px-3 py-1 border rounded-l-full border-slate-200' />
+                                <input type="text" name="cari" id="" className='ms-2 outline-none px-3 py-1 border rounded-l-full border-slate-200 dark:bg-slate-800 dark:border-slate-500' />
                                 <span className='bg-blue-600 text-white px-3 py-1 rounded-r-full'>
                                     <button type='submit'>
                                         <SearchIcon />
@@ -127,30 +131,45 @@ export default function Navbar() {
                                 </span>
                             </div>
                         </div>
-                        <div className="p-4">
-                            <label className="switch">
-                                <input type="checkbox" onChange={toggleDarkMode} />
-                                <span className="slider round"></span>
-                            </label>
-                        </div>
+                        <div className='flex items-center'>
+                            <div className="p-4 flex-1">
+                                <label className="switch cursor-pointer">
+                                    <input type="checkbox" onChange={toggleDarkMode} className='hidden' />
+                                    <span className={`slider round ${darkMode ? 'rotate-180' : ''}`}>
+                                        {darkMode ?
+                                            <span className='flex gap-2'>
+                                                <span><Brightness5OutlinedIcon /></span>
+                                                {/* <h1 className=''>Dark</h1> */}
+                                            </span>
+                                            :
+                                            <span className='flex gap-2'>
+                                                <NightlightOutlinedIcon />
+                                                
+                                            </span>
+                                        }
 
-                        <div>
-                            <div className='relative me-4'>
-                                <button>
-                                    <NotificationsNoneIcon sx={{ fontSize: 25 }} />
-                                </button>
-                                <p className='absolute block top-0 right-0 bg-red-600 rounded-full text-white text-xs'>12</p>
+                                    </span>
+                                </label>
                             </div>
 
+                            <div>
+                                <div className='relative me-4'>
+                                    <button>
+                                        <NotificationsNoneIcon sx={{ fontSize: 25 }} />
+                                    </button>
+                                    <p className='absolute block top-0 right-0 bg-red-600 rounded-full text-white text-xs'>12</p>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                    <div className={`daftar px-3 py-5 ${darkMode ? 'bg-slate-800' : 'bg-slate-100'} `}>
-                        {/* {isi === 'Shop' && <Shop />}
+                    <div className={`daftar px-3 py-5 bg-slate-100 dark:bg-slate-800`}>
+                        {isi === 'Shop' && <Shop />}
                         {isi === 'Analytic' && <Analytic />}
                         {isi === 'Tickets' && <Tickets />}
                         {isi === 'Users' && <Users />}
-                        {isi === 'Settings' && <Settings />} */}
-                        <Analytic theme={darkMode} />
+                        {isi === 'Settings' && <Settings />}
+                        {/* <Analytic theme={darkMode} /> */}
                     </div>
                 </div>
             </div>
